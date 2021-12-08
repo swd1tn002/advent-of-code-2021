babca015341d580723222af2be6b1f1782c69e77
Teemu H <swd1tn002@users.noreply.github.com>
2021-12-08 11:33:53 +0200

    Day 8 part 2

--- 08_seven_segment_search/search.py
+++ 08_seven_segment_search/search.py
@@ -37,19 +37,37 @@ def decode_notes(notes: List[Note]) -> List[List[int]]:
 
 
 def decode_note(note: Note) -> List:
-    decoded = dict([(decode_one(note.patterns), 1),
+    decoded = dict([(decode_zero(note.patterns), 0),
+                    (decode_one(note.patterns), 1),
+                    (decode_two(note.patterns), 2),
                     (decode_three(note.patterns), 3),
                     (decode_four(note.patterns), 4),
+                    (decode_five(note.patterns), 5),
+                    (decode_six(note.patterns), 6),
                     (decode_seven(note.patterns), 7),
                     (decode_eight(note.patterns), 8),
-                    (decode_nine(note.patterns), 9)
-                    ])
+                    (decode_nine(note.patterns), 9)])
 
     return [decoded[x] for x in note.output if x in decoded]
 
 
+def decode_zero(patterns: List[int]) -> str:
+    # Only remaining with length 6 is zero
+    len_six = filter_by_length(patterns, 6)
+    six = decode_six(patterns)
+    nine = decode_nine(patterns)
+    return [*(set(len_six) - {six, nine})][0]
+
+
 def decode_one(patterns: List[str]) -> str:
     return filter_by_length(patterns, 2)[0]
+
+
+def decode_two(patterns: List[str]) -> str:
+    len_five = filter_by_length(patterns, 5)
+    three = decode_three(patterns)
+    five = decode_five(patterns)
+    return [x for x in len_five if x != three and x != five][0]
 
 
 def decode_three(patterns: List[str]) -> str:
@@ -62,6 +80,22 @@ def decode_three(patterns: List[str]) -> str:
 def decode_four(patterns: List[str]) -> str:
     # 4 is the only digit that uses four segments
     return filter_by_length(patterns, 4)[0]
+
+
+def decode_six(patterns: List[str]) -> str:
+    # Number 6: length 6, but not 9. Does not contain all segments from 1.
+    len_six = filter_by_length(patterns, 6)
+    one = decode_one(patterns)
+    return [x for x in len_six if len(set(one) - set(x)) == 1][0]
+
+
+def decode_five(patterns: List[str]) -> str:
+    # Length is 5, but is not three. Contains a segment, that neither 7 nor 3 contain, unlike number 2
+    len_five = filter_by_length(patterns, 5)
+    four = set(decode_four(patterns))
+    three = set(decode_three(patterns))
+    top_left = four - three
+    return [x for x in len_five if len(top_left - set(x)) == 0][0]
 
 
 def decode_seven(patterns: List[str]) -> str:
@@ -86,9 +120,12 @@ def filter_by_length(patterns: List[str], length: int) -> List[str]:
 
 if __name__ == '__main__':
     notes = read_notes()
-    count = 0
+    total = 0
     for note in notes:
         decoded = decode_note(note)
         print(decoded)
-        count += len(decoded)
-    print(f'Count: {count}')
+
+        as_number = ''.join(map(str, decoded))
+        total += int(as_number)
+
+    print(f'Sum: {total}')
