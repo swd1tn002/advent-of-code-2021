0bb8b73d39008df74d0a8267737525299076e77a
Teemu H <swd1tn002@users.noreply.github.com>
2021-12-11 08:48:44 +0200

    Day 11 parts 1 and 2

---
+++ 11_dumbo_octopus/octopus.py
@@ -0,0 +1,109 @@
+
+import os
+from typing import List, Dict, Set
+from collections import namedtuple
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+Coord = namedtuple('Coord', 'x y')
+
+lines = """5421451741
+3877321568
+7583273864
+3451717778
+2651615156
+6377167526
+5182852831
+4766856676
+3437187583
+3633371586""".split('\n')
+
+
+def read_energy_levels(filename=INPUT_FILE) -> Dict[Coord, int]:
+    """
+    Each octopus has an energy level - your submarine can remotely measure 
+    the energy level of each octopus (your puzzle input)
+    """
+    with open(filename) as file:
+        return parse_energy_levels(file.readlines())
+
+
+def parse_energy_levels(input: List[str]) -> Dict[Coord, int]:
+    """
+    Parses the given list of strings into a dict with the x and y axis coordinates as
+    keys and respective numbers as values:
+    ["12", "65"] =>  { Coord(x=0, y=0): 1, Coord(x=1, y=0): 2, Coord(x=0, y=1): 6, Coord(x=1, y=1): 5 }
+    """
+    rows_and_cols = [line_to_ints(line) for line in input]
+    return {Coord(x, y): value for (y, row) in enumerate(rows_and_cols) for (x, value) in enumerate(row)}
+
+
+def line_to_ints(line: str) -> List[int]:
+    """
+    Converts string with digits into a list of single digit ints.
+    """
+    return list(map(int, line.strip()))
+
+
+def find_neighbors(grid: Dict[Coord, int], coord: Coord) -> Set[Coord]:
+    """
+    Get adjacent coordinates including ones that are diagonally adjacent.
+    """
+    x, y = coord
+    return {Coord(_x, _y) for (_x, _y) in ((x-1, y-1), (x+1, y-1), (x-1, y+1), (x+1, y+1), (x, y-1), (x, y+1), (x-1, y), (x+1, y)) if (_x, _y) in grid}
+
+
+def next_step(grid: Dict[Coord, int]) -> Dict[Coord, int]:
+    # First, the energy level of each octopus increases by 1.
+    new_grid = {c: level+1 for c, level in grid.items()}
+
+    # Then, any octopus with an energy level greater than 9 flashes. This increases
+    # the energy level of all adjacent octopuses by 1, including octopuses that are diagonally adjacent.
+    # If this causes an octopus to have an energy level greater than 9, it also flashes. This process
+    # continues as long as new octopuses keep having their energy level increased beyond 9.
+    flashed = set()
+    while any(level > 9 and coord not in flashed for coord, level in new_grid.items()):
+        flashing = {
+            coord for coord, value in new_grid.items() if value > 9 and coord not in flashed
+        }
+        for flash in flashing:
+            for neighbor in find_neighbors(new_grid, flash):
+                new_grid[neighbor] += 1
+
+        flashed = flashed | flashing
+
+    # Finally, any octopus that flashed during this step has its energy level set to 0, as it used
+    # all of its energy to flash.
+    return {c: level if level <= 9 else 0 for c, level in new_grid.items()}, len(flashed)
+
+
+def test_print(grid: Dict[Coord, int]) -> None:
+    w = max(c.x for c in grid.keys()) + 1
+    h = max(c.y for c in grid.keys()) + 1
+
+    for y in range(h):
+        for x in range(w):
+            print(grid[Coord(x, y)], end='')
+        print('')
+    print('')
+
+
+if __name__ == '__main__':
+    # Part 1
+    grid = parse_energy_levels(lines)
+    flashes = 0
+    for i in range(100):
+        grid, flashed = next_step(grid)
+        flashes += flashed
+
+    test_print(grid)
+
+    print(f'Part 1: flashed {flashes} times')
+
+    # Part 2
+    grid = parse_energy_levels(lines)
+    for i in range(1, 1_000):
+        grid, flashed = next_step(grid)
+        if all(value == 0 for value in grid.values()):
+            print(f'Part 2: Synchronized after {i} steps!')
+            break
