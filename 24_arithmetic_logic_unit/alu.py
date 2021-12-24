03042ef93f939365e8fe353c0bc606eb8f85345a
Teemu H <swd1tn002@users.noreply.github.com>
2021-12-24 22:23:47 +0200

    Added comment to solve function

--- 24_arithmetic_logic_unit/alu.py
+++ 24_arithmetic_logic_unit/alu.py
@@ -8,8 +8,25 @@ solvers = (solver0, solver1, solver2, solver3, solver4, solver5, solver6,
 
 
 @lru_cache(maxsize=100_000)
-def solve(x, y, z, funcs: Tuple[Callable], order: Tuple[int]):
+def solve(x, y, z, funcs: Tuple[Callable], order: Tuple[int]) -> Tuple:
+    """
+    This function is executed recursively 14 times. Each time corresponds to a single
+    digit in a 14 digit serial number being validated. For each digit the function sets
+    all accepted values in the given `order`, calls the sub function that processes that
+    input value and passes the results along recursively to try all next combinations.
+
+    If all the functions are consumed, there is no more digits to try, and this function 
+    determines if the end value for `z` is 0 (a valid serial number). If so, `True` and
+    the digits leading to the outcome are catenated and returned.
+
+    If no recursive calls are successful, this function returns (False, None).
+    """
+
     if not funcs:
+        """
+        **After MONAD has finished running all of its instructions, it will indicate that
+        the model number was valid by leaving a 0 in variable z.**
+        """
         return z == 0, ''
 
     func, *rest = funcs
