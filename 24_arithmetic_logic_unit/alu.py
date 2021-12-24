bfe573921c53731f2dd506a0b35f7144abdaa511
Teemu H <swd1tn002@users.noreply.github.com>
2021-12-24 18:59:45 +0200

    Day 24 parts 1 and 2 working. Code still somewhat messy.

--- 24_arithmetic_logic_unit/alu.py
+++ 24_arithmetic_logic_unit/alu.py
@@ -1,380 +1,366 @@
-from multiprocessing import Process
 from typing import List, Dict, Tuple
+from functools import lru_cache
 
 
-class ALU:
-    def __init__(self, instructions: List[str]):
-        self.instructions = instructions
-
-    def process(self, vars: Dict[str, int], inp: List[int]) -> Dict[str, int]:
-        for instr in self.instructions:
-            self._evaluate(instr, vars, inp)
-
-        return vars
-
-    def _evaluate(self, instr: str, vars: Dict[str, int], inp: List[int]):
-        command, *params = instr.split()
-        func = getattr(self, f'_{command}')
-        func(vars, inp, params)
-
-    def _inp(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        inp a - Read an input value and write it to variable a.
-        """
-        vars[params[0]] = inp.pop(0)
-
-    def _add(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        add a b - Add the value of a to the value of b, then store the result in variable a.
-        """
-        a, b = params
-        if b not in vars:
-            b = int(b)
-        else:
-            b = vars[b]
-
-        vars[a] += b
-
-    def _mul(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        mul a b - Multiply the value of a by the value of b, then store the result in variable a.
-        """
-        a, b = params
-        if b not in vars:
-            b = int(b)
-        else:
-            b = vars[b]
-        vars[a] *= b
-
-    def _div(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        div a b - Divide the value of a by the value of b, truncate the result to an integer,
-        then store the result in variable a. (Here, "truncate" means to round the value toward zero.)
-        """
-        a, b = params
-        if b not in vars:
-            div = int(b)
-        else:
-            div = vars[b]
-        vars[a] = vars[a] // div
-
-    def _mod(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        mod a b - Divide the value of a by the value of b, then store the remainder in variable a.
-        """
-        a, b = params
-        if b not in vars:
-            mod = int(b)
-        else:
-            mod = vars[b]
-        vars[a] = vars[a] % mod
-
-    def _eql(self, vars: Dict[str, int], inp: List[int], params: Tuple[str]):
-        """
-        eql a b - If the value of a and b are equal, then store the value 1 in variable a. 
-        Otherwise, store the value 0 in variable a.
-        """
-        a, b = params
-        if b not in vars:
-            val = int(b)
-        else:
-            val = vars[b]
-        vars[a] = int(vars[a] == val)
+# @lru_cache(maxsize=100_000)
+def solver0(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 10
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 12
+    y = y * x
+    z = z + y
+    return w, x, y, z
 
 
-lines = """inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 10
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 12
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 10
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 10
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 12
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 8
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 11
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 4
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x 0
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 3
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 15
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 10
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 13
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 6
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -12
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 13
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -15
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 8
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -15
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 1
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -4
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 7
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 1
-add x 10
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 6
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -5
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 9
-mul y x
-add z y
-inp w
-mul x 0
-add x z
-mod x 26
-div z 26
-add x -12
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 9
-mul y x
-add z y"""
-
-counter = 0
+# @lru_cache(maxsize=100_000)
+def solver1(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 10
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 10
+    y = y * x
+    z = z + y
+    return w, x, y, z
 
 
-def solve(vars: Dict[str, int], programs: List[ALU]):
-    if not programs:
-        global counter
-        counter += 1
-        if counter % 1_000 == 0:
-            print(counter)
+# @lru_cache(maxsize=100_000)
+def solver2(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 12
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 8
+    y = y * x
+    z = z + y
+    return w, x, y, z
 
-        """
-        ...it will indicate that the model number was valid by leaving a 0 in variable z...
-        """
-        return vars['z'] == 0, ''
 
-    p = programs[0]
+# @lru_cache(maxsize=100_000)
+def solver3(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 11
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 4
+    y = y * x
+    z = z + y
+    return w, x, y, z
 
-    for n in range(9, 0, -1):
-        result = p.process(vars.copy(), [n])
-        found, suffix = solve(result, programs[1:])
+
+# @lru_cache(maxsize=100_000)
+def solver4(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + 0
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 3
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver5(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 15
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 10
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver6(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 13
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 6
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver7(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -12
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 13
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver8(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -15
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 8
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver9(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -15
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 1
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver10(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -4
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 7
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver11(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 1
+    x = x + 10
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 6
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver12(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -5
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 9
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+# @lru_cache(maxsize=100_000)
+def solver13(w, x, y, z):
+    x = x * 0
+    x = x + z
+    x = x % 26
+    z = z // 26
+    x = x + -12
+    x = int(x == w)
+    x = int(x == 0)
+    y = y * 0
+    y = y + 25
+    y = y * x
+    y = y + 1
+    z = z * y
+    y = y * 0
+    y = y + w
+    y = y + 9
+    y = y * x
+    z = z + y
+    return w, x, y, z
+
+
+solvers = (
+    solver0,
+    solver1,
+    solver2,
+    solver3,
+    solver4,
+    solver5,
+    solver6,
+    solver7,
+    solver8,
+    solver9,
+    solver10,
+    solver11,
+    solver12,
+    solver13
+)
+
+
+@lru_cache(maxsize=100_000)
+def solve(x, y, z, funcs: Tuple):
+    if not funcs:
+        return z == 0, ''
+
+    func, *rest = funcs
+
+    for i in range(1, 10):
+        _, _x, _y, _z = func(i, x, y, z)
+
+        found, suffix = solve(_x, _y, _z, tuple(rest))
 
         if found:
-            return True, str(n) + suffix
+            return True, str(i) + suffix
 
     return False, None
 
 
-if __name__ == '__main__':
-    # Split into sub programs which all use only one input value
-    blocks = lines.split('\ninp')
-    blocks = [blocks[0]] + ['inp' + block for block in blocks[1:]]
+print(solve(x=0, y=0, z=0, funcs=solvers))
 
-    programs: List[ALU] = []
+# if __name__ == '__main__':
+#     # Split into sub programs which all use only one input value
+#     blocks = lines.split('\ninp')
+#     blocks = [blocks[0]] + ['inp' + block for block in blocks[1:]]
 
-    for block in blocks:
-        programs.append(ALU(block.split('\n')))
+#     programs: List[ALU] = []
 
-    # Solve use the given programs recursively to find the highest input combination
-    # that is a valid serial number
-    solution = solve({'w': 0, 'x': 0, 'y': 0, 'z': 0}, programs)
-    print(solution)
+#     for block in blocks:
+#         programs.append(ALU(block.split('\n')))
 
-    # Start from 99999995597000
+#     # Solve use the given programs recursively to find the highest input combination
+#     # that is a valid serial number
+#     solution = solve({'w': 0, 'x': 0, 'y': 0, 'z': 0}, programs)
+#     print(solution)
+
+#     # Start from 99999995597000
---
+++ 24_arithmetic_logic_unit/alu_language.py
@@ -0,0 +1,279 @@
+script = """inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 12
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 10
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 12
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 8
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 11
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 4
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x 0
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 3
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 10
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 6
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -12
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 13
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 8
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -15
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 1
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -4
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 7
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 10
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 6
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -5
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 9
+mul y x
+add z y
+inp w
+mul x 0
+add x z
+mod x 26
+div z 26
+add x -12
+eql x w
+eql x 0
+mul y 0
+add y 25
+mul y x
+add y 1
+mul z y
+mul y 0
+add y w
+add y 9
+mul y x
+add z y""".split('\n')
+
+i = 0
+for line in script:
+    c, *rest = line.split(' ')
+    if c == 'inp':
+        if i > 0:
+            print('    return w, x, y, z\n\n')
+        print(f'def solver{i}(w, x, y, z):')
+        i += 1
+    else:
+        try:
+            c, a, b = line.split(' ')
+            if c == 'add':
+                print(f'    {a} += {b}')
+            elif c == 'mul':
+                print(f'    {a} *= {b}')
+            elif c == 'div':
+                print(f'    {a} //= {b}')
+            elif c == 'mod':
+                print(f'    {a} %= {b}')
+            elif c == 'eql':
+                print(f'    {a} = int({a} == {b})')
+            else:
+                raise Exception(line)
+        except:
+            raise Exception(line)
+print('    return w, x, y, z\n\n')
